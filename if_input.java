a85af1226497a56fccad6d725c0aba0cc98ef17b
anishmrao <anish01123@gmail.com>
2019-03-17 22:11:23 +0530

    Added Phase2 code

--- if_input.java
+++ if_input.java
@@ -3,13 +3,18 @@ class IfStatement {
 
     	int number = 10;
     	// System.out.println("Number is positive.");
-    	if (number > 0){
+    	if (number > 0)
+        {
     		number = 5;
      	}
-     	else
+     	else if(number==0)
      	{
      		number = 4;
      	}
+        else
+        {
+            number = 2;
+        }
     	// System.out.println("This statement is always executed.");
     }
 }
---
+++ phase2.py
@@ -0,0 +1,388 @@
+import random
+import sys
+
+sys.path.insert(0, "../..")
+
+scope_level = -1
+
+if sys.version_info[0] >= 3:
+    raw_input = input
+
+reserved = {
+    'public' : 'PUBLIC',
+    'static': 'STATIC',
+    'void': 'VOID',
+    'main' : 'MAIN',
+    'class' : 'CLASS',
+    'String' : 'STRING',
+    'args' : 'ARGS',
+    'int' : 'INT',
+    'float' : 'FLOAT',
+    'boolean' : 'BOOLEAN',
+    'false' : 'FALSE',
+    'true' : 'TRUE',
+    'if' : 'IF',
+    'else' : 'ELSE',
+    'private' : 'PRIVATE',
+    'protected' : 'PROTECTED',
+    'do' : 'DO',
+    'while' : 'WHILE',
+}
+
+tokens = (
+    'NAME', 'NUMBER', 'FLOATNUMBER', 'REL_EQ', 'REL_NEQ', 'REL_GTEQ', 'REL_LTEQ','MINEQ','PLUSEQ','DIVEQ','MULEQ',
+    'LBRACE', 'RBRACE'
+) + tuple(reserved.values());
+
+literals = ['=', '+', '-', '*', '/', '(', ')', '<', '>', '!','{','}' ,';', ':']
+
+# Tokens
+# t_multicomment = r'/\*.*\n.*\*/'
+t_REL_EQ = r'=='
+t_REL_GTEQ = r'>='
+t_REL_LTEQ = r'<='
+t_REL_NEQ = r'!='
+t_MINEQ = r'-='
+t_PLUSEQ = r'\+='
+t_DIVEQ = r'/='
+t_MULEQ = r'\*='
+# t_comment = r'//.*'
+
+t_ignore = " \t[]"
+def t_comment_ignore(t):
+    r"//.*|/\*+[^*]*\*+(?:[^/*][^*]*\*+)*/"
+
+
+def t_NAME(t):
+    r'[a-zA-Z_][a-zA-Z0-9_]*'
+    t.type = reserved.get(t.value,'NAME')
+    print(t)
+    return t
+
+def t_FLOATNUMBER(t):
+    r'[0-9]*[.][0-9]+'
+    # print(t.value)
+    t.value = float(t.value)
+    # print("In floatnumber", t.value)
+    return t
+
+def t_NUMBER(t):
+    r'\d+'
+
+    t.value = int(t.value)
+    return t
+
+
+# def t_FLOATNUMBER(t):
+
+
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += t.value.count("\n")
+    # print(t.lexer.lineno)
+
+def t_LBRACE(t):
+    r'{'
+    # print(t)
+    global scope_level
+    scope_level += 1
+    # print(t, scope_level)
+    return t
+
+def t_RBRACE(t):
+    r'}'
+    # global scope_level
+    # scope_level -= 1
+    # print("Reduced here", t)
+    return t
+
+def t_error(t):
+    print("Illegal character '%s'"  %t.value)
+    t.lexer.skip(1)
+
+# Build the lexer
+import ply.lex as lex
+lexer = lex.lex()
+
+
+# lexer.input(s)
+
+# Parsing rules
+
+precedence = (
+    ('left','='),
+    ('left','REL_EQ','REL_NEQ'),
+    ('left', '<','>','REL_GTEQ', 'REL_LTEQ'),
+    ('left', '+', '-'),
+    ('left', '*', '/'),
+    ('right', 'UMINUS','!'),
+)
+
+# dictionary of names
+symbol_table = dict()
+names = {}
+start = 'start'
+
+def p_start(p):
+    ''' start : PUBLIC CLASS NAME LBRACE statements main_body RBRACE
+              | CLASS NAME LBRACE statements main_body RBRACE'''
+
+def p_main_body(p):
+    ''' main_body : PUBLIC STATIC VOID MAIN '(' STRING ARGS ')' LBRACE body RBRACE 
+                  | body '''
+
+def p_body(p):
+    ''' body : statements '''
+
+def p_statements(p):
+    ''' statements : statements statement 
+                   | '''
+def p_blockstatemet(p):
+    ''' block_statement : LBRACE statements RBRACE '''
+    global scope_level
+    scope_level-=1
+
+def p_statement_do_while(p):
+    ''' statement : DO block_statement WHILE '(' expression ')' ';' '''
+# num_called = 0;
+
+def p_else_if_block(p):
+    ''' else_if : ELSE IF '(' expression ')' block_statement else_if
+                | ELSE IF '(' expression ')' statement else_if 
+                | ELSE block_statement
+                | ELSE statement
+                | '''
+    # print("HEREEEEEEE")
+    # global num_called
+    # num_called+=1
+
+def p_statement_if(p):
+    ''' statement : IF '(' expression ')' block_statement else_if
+                  | IF '(' expression ')' statement else_if '''
+    # global scope_level
+    # global num_called
+    # scope_level -= num_called + 1
+    # num_called = 0
+    # print("Reduced at", p.lexer.lineno)
+  
+
+def p_statement_declare(p):
+    ''' statement : INT NAME ';'
+                  | FLOAT NAME ';'
+                  | BOOLEAN NAME ';' '''
+    # print("In p_statement_declare", p.slice)
+    global scope_level
+    if p[1] == "int" :
+        names[p[2]] = 0
+    elif p[1] == "float":
+        names[p[2]] = 0.0
+    elif p[1] == "boolean":
+        names[p[2]] = "False"
+    if scope_level == 0:
+        scope = 'global'
+    else:
+        scope = 'scope'+str(scope_level)
+    token = [p.slice[2].type, p.slice[2].value, p.lexer.lineno - 1,p[1],scope,names[p[2]]]
+    if(scope_level not in symbol_table.keys()):
+        symbol_table[scope_level] = dict()
+    symbol_table[scope_level][p[2]+"_"+str(p.lexer.lineno - 1)] = token
+
+# def p_statement_assign(p):
+#     '''statement : NAME "=" expression ';' '''
+#     print("In p_statement_assign", p.slice)
+#     print(p[2])
+
+def p_statement_init(p):
+    '''statement : NAME "=" expression ';'
+                 | INT NAME "=" expression ';'
+                 | FLOAT NAME "=" expression ';'
+                 | BOOLEAN NAME "=" TRUE ';'
+                 | BOOLEAN NAME "=" FALSE ';' '''
+    
+    global scope_level
+    # print("STATEMENT")
+    # print(symbol_table, scope_level)
+    # print("In p_statement_init", p.slice)
+    # print(p[2])
+    if scope_level == 0:
+        scope = 'global'
+    else:
+        scope = 'scope'+str(scope_level)
+    if(scope_level not in symbol_table.keys()):
+            symbol_table[scope_level] = dict()
+    if(p[2] == '='):
+        # print(symbol_table)
+        # token = [p.slice[2].type, p.slice[2].value, p.lexer.lineno,p[1],scope, 'placeholder']
+        token = None
+        for s in range(scope_level, -1, -1):
+            flag = 0
+            for i in symbol_table[s].keys():
+                # print(len(i.split('_')[0]), len(p[1]))
+                if(i.split('_')[0]==p[1]):
+                    token = symbol_table[s][i].copy()
+                    token[4] = scope
+                    token[2] = p.lexer.lineno - 1
+                    # print(key_p)
+                    flag = 1
+                    break
+            if(flag):
+                break
+
+
+            # if(p[1] in symbol_table[s].keys()):
+            #     token = symbol_table[s][key_p].copy()
+            #     token[4] = scope
+            #     token[2] = p.lexer.lineno - 1
+            #     break
+        if(not token):
+            print("Token "+p[1]+" not found")
+        
+        key = p[1]+"_"+str(p.lexer.lineno-1)
+        symbol_table[scope_level][key] = token
+        if (type(p[3]) == bool ):
+            if symbol_table[scope_level][key][3] == "boolean":
+                names[p[1]] = p[3]
+                symbol_table[scope_level][key][5] = names[p[1]]
+            else:
+                print("Type error : assignment not supported")
+        elif (type(p[3]) == int ) :
+            if symbol_table[scope_level][key][3] == "int":
+                names[p[1]] = p[3]
+                symbol_table[scope_level][key][5] = names[p[1]]
+            elif symbol_table[scope_level][key][3] == "float":
+                # print("Here")
+                names[p[1]] = float(p[3])
+                symbol_table[scope_level][key][5] = names[p[1]]
+            else:
+                print("Type error : assignment not supported")
+        elif ( type(p[3]) == float ) :
+            if symbol_table[scope_level][key][3] == "float":
+                names[p[1]] = p[3]
+                symbol_table[scope_level][key][5] = names[p[1]]
+            else:
+                print("Type error : assignment not supported")
+    else:   
+        # print("Printing p")
+        # for i in p:
+        #     print(i)
+        key = p[2]+"_"+str(p.lexer.lineno-1)
+        symbol_table[scope_level][key] = [p.slice[2].type, p.slice[2].value, p.lexer.lineno - 1,p[1],scope, 'placeholder']
+        names[p[2]] = p[4]
+        symbol_table[scope_level][key][5] = names[p[2]]
+        symbol_table[scope_level][key][3] = p[1]
+
+
+def p_statement_expr(p):
+    '''statement : expression ";" '''
+    print(p[1])
+
+def p_expr_boolean(p):
+    '''expression : TRUE
+                  | FALSE '''
+    p[0] = p[1]
+
+def p_expression_binop(p):
+    '''expression : expression '+' expression
+                  | expression '-' expression
+                  | expression '*' expression
+                  | expression '/' expression'''
+    if p[2] == '+':
+        p[0] = p[1] + p[3]
+    elif p[2] == '-':
+        p[0] = p[1] - p[3]
+    elif p[2] == '*':
+        p[0] = p[1] * p[3]
+    elif p[2] == '/':
+        p[0] = p[1] / p[3]
+
+def p_statement_relational(p):
+    '''expression : expression '>' expression
+                 | expression '<' expression''' 
+    if p[2] == '>':
+        p[0] = (p[1] > p[3])
+    elif p[2] == '<':
+        p[0] = (p[1] < p[3])
+
+def p_relop(p):
+    '''expression : expression REL_EQ expression
+              | expression REL_NEQ expression
+                      | expression REL_GTEQ expression
+              | expression REL_LTEQ expression'''
+    if p[2] == "==" :
+        p[0] = (p[1] == p[3])
+    elif p[2] == "!=" : 
+        p[0] = (p[1] != p[3])
+    elif p[2] == "<=" : 
+        p[0] = (p[1] <= p[3])
+    elif p[2] == ">=" :
+        p[0] = (p[1] >= p[3])
+   
+
+def p_operation_equals(p):
+    '''statement : NAME MINEQ expression ";"
+                 | NAME PLUSEQ expression ";"
+                 | NAME DIVEQ expression ";"
+                 | NAME MULEQ expression ";" '''
+    print("In p_operation_equals", p.slice)
+    if p[2] == '+=':
+        names[p[1]] = names[p[1]] + p[3]
+        symbol_table[scope_level][p[1]][5] = names[p[1]]
+    elif p[2] == '-=':
+        names[p[1]] = names[p[1]] - p[3]
+        symbol_table[scope_level][p[1]][5] = names[p[1]]
+    elif p[2] == '/=':
+        names[p[1]] = names[p[1]] / p[3]
+        symbol_table[scope_level][p[1]][5] = names[p[1]]
+    elif p[2] == '*=':
+        names[p[1]] = names[p[1]] * p[3]
+        symbol_table[scope_level][p[1]][5] = names[p[1]]
+
+def p_expression_not(p):
+    '''expression : '!' expression'''
+    p[0] = not (p[2])
+
+def p_expression_uminus(p):
+    '''expression : '-' expression %prec UMINUS'''
+    p[0] = -p[2]
+
+def p_expression_group(p):
+    '''expression : '(' expression ')' '''
+    p[0] = p[2]
+
+
+def p_expression_number(p):
+    '''expression : FLOATNUMBER
+                  | NUMBER'''
+    p[0] = p[1]
+
+
+def p_expression_name(p):
+    '''expression : NAME'''
+    try:
+        p[0] = names[p[1]]
+    except LookupError:
+        print("Undefined name '%s'" % p[1])
+        p[0] = 0
+
+
+def p_error(p):
+    if p:
+        print("Syntax error at '%s'" % p.lineno)
+    else:
+        print("Syntax error at EOF")
+
+import ply.yacc as yacc
+parser = yacc.yacc()
+
+
+try:
+    f = open(sys.argv[1],"r")
+    s=f.read()
+except:
+    print("Please enter input filename as argument.")
+    sys.exit(1)
+
+parser = yacc.yacc()
+parser.parse(s)
+# print("Here")
+print(symbol_table)
